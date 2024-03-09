53bdceec640024f1109d55c13855b14132076a08
David Corticchiato <david.corticchiato@gmail.com>
2024-03-08 19:26:05 -0500

    show game scene on joined

--- scenes/menus/menus.gd
+++ scenes/menus/menus.gd
@@ -9,9 +9,15 @@ const GAME_NAME = 'parasite'
 
 
 func _ready():
+    Immersive.client.joined.connect(on_game_room_join)
+    
     create_button.pressed.connect(create_game_room)
     join_button.pressed.connect(join_game_room)
 
+func on_game_room_join(game_room_code: String, client_id: int):
+    var game = game_scene.instantiate()
+    get_tree().root.add_child(game)
+    queue_free()
 
 func create_game_room():
     Immersive.client.book_game_room(GAME_NAME)
