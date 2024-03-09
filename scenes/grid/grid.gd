c8e01ec2c418655b83bfd29836e2e397cd251e43
unknown <martinbriands@gmail.com>
2024-03-09 16:52:15 -0500

    added images
    wow pretty

--- scenes/grid/grid.gd
+++ scenes/grid/grid.gd
@@ -3,15 +3,23 @@ extends Node2D
 class_name Grid
 
 @export var tile_map: TileMap
+@export var selection_tile_map: TileMap
+
 signal cell_click
 
 var grid_size
+
+var cardinal = [
+    Vector2.UP,
+    Vector2.DOWN,
+    Vector2.RIGHT,
+    Vector2.LEFT
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     grid_size = tile_map.tile_set.tile_size.x
     pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -28,3 +36,15 @@ func get_grid_pos(position: Vector2):
 
 func get_screen_pos(grid_pos: Vector2):
     return Vector2(grid_pos.x, grid_pos.y) * grid_size
+    
+func show_possible_selection(grid_pos: Vector2):
+    for direction in cardinal:
+        var pos = Vector2(grid_pos + direction)
+        if is_possible_tile(pos):
+            selection_tile_map.set_cell(0, pos, 0, Vector2(0,0))
+
+func clear_possible_selections():
+    selection_tile_map.clear()
+    
+func is_possible_tile(grid_pos: Vector2):
+    return true
