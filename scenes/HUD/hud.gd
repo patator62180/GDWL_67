cce5d9dd2473dee9951ac68cca98a036f0f23d6f
patator62180 <samuel.terrasviola@gmail.com>
2024-03-10 17:32:06 -0400

    fix hud merge

--- scenes/HUD/hud.gd
+++ scenes/HUD/hud.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
 @export var player_cards: Array[PlayerCard]
+@export var PlayerManager0: PlayerManager
+@export var PlayerManager1: PlayerManager
+
 
 func set_winning_label(isWinning : bool):
     $YouWinLabel.visible = isWinning
