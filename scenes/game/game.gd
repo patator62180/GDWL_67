e461c82eb70c40f1e6cac7e3d1930c2467d8cdc3
patator62180 <samuel.terrasviola@gmail.com>
2024-03-10 16:54:34 -0400

    lose text replicated

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -8,6 +8,7 @@ const MAX_PLAYERS_COUNT = 4
 @export var player_cards: Array[PlayerCard]
 @export var grid: Grid
 @export var you_won_label: Label
+@export var you_lost_label: Label
 
 var players = []
 var hosts: Array[Host] = []
@@ -27,6 +28,7 @@ func _ready():
 
     grid.cell_click.connect(on_cell_click)
     grid.wall_click.connect(on_wall_click)
+    you_won_label.visible = false
 
 func on_player_played():
     check_if_player_won()
@@ -41,7 +43,8 @@ func check_if_player_won():
         var current_player_manager = player_characters[player_index_playing]
         for player in current_player_manager.player_characters:
             if(host.position == player.position):
-                you_won_label.visible = true
+                winning_player.rpc(player_index_playing)
+                
 
 func set_turn(player_index: int):
     propagate_turn.rpc(player_index)
@@ -57,6 +60,11 @@ func is_player_active_turn():
 @rpc('authority')
 func propagate_turn(player_index: int):
     player_index_playing = player_index
+
+@rpc('authority')
+func winning_player(player_index: int):
+    you_won_label.visible = is_player_active_turn()                
+    you_lost_label.visible = !is_player_active_turn()
 
 @rpc('any_peer')
 func start_game():
