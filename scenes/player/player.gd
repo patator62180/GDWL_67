9f9d438dcbc8035474643b681793d9b37413f86e
unknown <martinbriands@gmail.com>
2024-03-09 20:41:47 -0500

    player moves over time

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -8,6 +8,13 @@ var tile_size = 100
 
 var grid_pos = Vector2.ZERO
 
+var is_moving = false
+var start_pos = Vector2.ZERO
+var target_pos = Vector2.ZERO
+
+var timer = 0.0
+var timer_max = 0.25
+
 @export var raycast: RayCast2D
 
 func move(direction):
@@ -20,8 +27,20 @@ func move(direction):
 @rpc("any_peer")
 func move_to(position: Vector2):
     if multiplayer and multiplayer.is_server():
-        self.position = position
-        played.emit()
+        is_moving = true
+        start_pos = self.position
+        target_pos = position
+        timer = 0
+        
+func _process(delta):
+    if is_moving and multiplayer and multiplayer.is_server():
+        self.position = lerp(start_pos, target_pos, timer/timer_max)
+        timer += delta
+        
+        if timer >= timer_max:
+            self.position = target_pos
+            is_moving = false
+            played.emit()
         
 func can_move_to(grid_pos: Vector2):
     return self.grid_pos.distance_to(grid_pos) <= 1
