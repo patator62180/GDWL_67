b37fc5471c171b76fae90f9c2e8acb3f670ab0d0
LeTitouLapinou <162844593+LeTitouLapinou@users.noreply.github.com>
2024-03-11 10:46:53 -0400

    ajout variable score
    La variable est stockee dans ScoreControl.gd pour l'instant

--- scenes/game/ScoreControl.gd
+++ scenes/game/ScoreControl.gd
@@ -1,6 +1,10 @@
 extends Control
 
+@export var scoreJ1 = 0
+@export var scoreJ2 = 0
 
 func _ready():
     %"Score J1".material.set_shader_parameter("Shift_Hue", get_parent().PlayerManagers.array[0].modulateFaceColor)
+    %"Score J1".text = "Player 1: " + str(scoreJ1) + " points"
     %"Score J2".material.set_shader_parameter("Shift_Hue", get_parent().PlayerManagers.array[1].modulateFaceColor)
+    %"Score J2".text = "Player 2: " + str(scoreJ2) + " points"
