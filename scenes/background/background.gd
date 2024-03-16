5c58d7e020b65eb3e79a4cf11bf321183e3a9cc0
patator62180 <samuel.terrasviola@gmail.com>
2024-03-16 17:06:39 -0400

    Correction menu j'adore le caca miam miam miam

--- scenes/background/background.gd
+++ scenes/background/background.gd
@@ -1,7 +1,9 @@
 extends Sprite2D
 
 func _process(delta):
-    if PlayerController.instance.can_play() and not OS.has_feature('dedicated_server'):
+    if PlayerController.instance == null:
+        material.set_shader_parameter("tint_color", Color(1,1,1,1))
+    elif  PlayerController.instance.can_play() and not OS.has_feature('dedicated_server'):
         material.set_shader_parameter("tint_color", Color(1,1,1,1))
     else:
         material.set_shader_parameter("tint_color", Color(1,0.3,0.3,1))
